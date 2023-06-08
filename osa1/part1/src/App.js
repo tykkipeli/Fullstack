d8ebed3bc3b848fb9d06eeb185885d75b326fe64
tykkipeli <olli.e.matilainen@helsinki.fi>
2023-06-08 21:14:30 +0300

    Create osa1

---
+++ osa1/part1/src/App.js
@@ -0,0 +1,29 @@
+const Hello = (props) => {
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear()
+    return yearNow - props.age
+  }
+
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+      <p>So you were probably born {bornYear()}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello />
+      <Hello />
+      <Hello />
+    </div>
+  )
+}
+
+
+export default App
